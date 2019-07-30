43325c8dafc730b829cecdc581ce17369e92808e
Garefen <gabrielbsarti@gmail.com>
2019-07-30 13:18:10 -0300

    Updates

---
+++ precache-manifest.d8362775f61dcf1437af70a5f2d3894d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc164fdb40ca184f6d5cc292c072be5",
+    "url": "/Share/index.html"
+  },
+  {
+    "revision": "021386093768c127f600",
+    "url": "/Share/static/css/main.109aa162.chunk.css"
+  },
+  {
+    "revision": "ba4e919d3a79660b60b3",
+    "url": "/Share/static/js/2.f600d728.chunk.js"
+  },
+  {
+    "revision": "021386093768c127f600",
+    "url": "/Share/static/js/main.a6a59217.chunk.js"
+  },
+  {
+    "revision": "e61a5a574d3f1f2a563f",
+    "url": "/Share/static/js/runtime~main.eeb44551.js"
+  },
+  {
+    "revision": "1146debfc486de48e2f31ac02dea7c8d",
+    "url": "/Share/static/media/leaves.1146debf.jpg"
+  },
+  {
+    "revision": "755e33f345dfe6a5fe1a2b6c8fdc7fe9",
+    "url": "/Share/static/media/logo.755e33f3.png"
+  },
+  {
+    "revision": "d7822bdfd03007ae3e010dc92375646f",
+    "url": "/Share/static/media/profile.d7822bdf.jpg"
+  }
+]);
