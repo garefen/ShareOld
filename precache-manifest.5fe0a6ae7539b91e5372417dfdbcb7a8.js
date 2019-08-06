8aaa4c6d4201cb571aed6816862bf202224763e5
Garefen <gabrielbsarti@gmail.com>
2019-08-06 15:54:32 -0300

    Updates

--- precache-manifest.5fe0a6ae7539b91e5372417dfdbcb7a8.js
+++ precache-manifest.5fe0a6ae7539b91e5372417dfdbcb7a8.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c847c869b6385b783a88c976cdf32077",
+    "revision": "f04d538eaff8281b7ad342fee6989468",
     "url": "/Share/index.html"
   },
   {
-    "revision": "3f23d1bc6186d5f092ba",
-    "url": "/Share/static/css/main.109aa162.chunk.css"
+    "revision": "7f558ff0994a8cd8dec2",
+    "url": "/Share/static/css/main.b5d43199.chunk.css"
   },
   {
     "revision": "ba4e919d3a79660b60b3",
     "url": "/Share/static/js/2.f600d728.chunk.js"
   },
   {
-    "revision": "3f23d1bc6186d5f092ba",
-    "url": "/Share/static/js/main.97b32f6b.chunk.js"
+    "revision": "7f558ff0994a8cd8dec2",
+    "url": "/Share/static/js/main.54e4e06d.chunk.js"
   },
   {
     "revision": "e61a5a574d3f1f2a563f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Share/precache-manifest.f6cf0d9a35926ac31ce3d951bd9561a0.js"
+  "/Share/precache-manifest.5fe0a6ae7539b91e5372417dfdbcb7a8.js"
 );
 
 self.addEventListener('message', (event) => {
